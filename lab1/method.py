e794f3522d46dc478d9bbeef6447010677825797
Dan <you@example.com>
2022-02-25 09:12:29 +0300

    Начата работа над методом

--- lab1/method.py
+++ lab1/method.py
@@ -1,2 +1,18 @@
+def diagonal_dominating(matrix):
+    summary = 0
+    flag = True
+
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            if i != j:
+                summary = summary + matrix[i][j]
+    print(summary)
+    for i in range(0, len(matrix)):
+        if matrix[i][i] < summary:
+            flag = False
+            break
+    return flag
+
+
 def calc(matrix, column, accuracy):
-    print(matrix)
+    print(diagonal_dominating(matrix))
